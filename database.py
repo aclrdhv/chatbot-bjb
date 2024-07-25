45324c618cba5c47ab7c49ae757ab22ec8abae9b
aclrdhv <marcellvirgiano@gmail.com>
2024-07-25 16:14:26 +0700

    database: make function fetch api

--- database.py
+++ database.py
@@ -12,3 +12,12 @@ def get_connection():
     cursor_factory=RealDictCursor
   )
   return conn
+
+def fetch_api_info(api_keyword):
+  conn = get_connection()
+  cur = conn.cursor()
+  cur.execute("SELECT * FROM api_specification WHERE keyword = %s", (api_keyword,))
+  result = cur.fetchone()
+  cur.close()
+  conn.close()
+  return result
